1a4a4d62b6c5dea083fb952377c5688ec8324fd4
Michael Holroyd <meekohi@gmail.com>
2014-07-30 14:12:41 -0400

    visualize streetview crawl

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 (function () {
 var m = L.map('mapID').setView([38.035, -78.495], 14);
+var arqgram = L.tileLayer('http://localhost/~meekohi/instagramtiles3/{z}/{x}/{y}.png',{zIndex:5}).addTo(m);
+var arqstreet = L.tileLayer('http://localhost/~meekohi/streetviewtiles3/{z}/{x}/{y}.png',{zIndex:4}).addTo(m);
+//var arqgram2 = L.tileLayer('http://localhost/~meekohi/instagramtiles2/{z}/{x}/{y}.png',{zIndex:4}).addTo(m);
+//var arqworld = L.tileLayer('https://{s}.tiles.mapbox.com/v3/mapbox.world-black/{z}/{x}/{y}.png',{zIndex:3}).addTo(m);
+
 var baseMaps = [
   "MapQuestOpen.OSM",
-  // "Stamen.Watercolor",
+  "Stamen.TonerLite"
+  //"HERE.normalNightGrey"
+  //"Stamen.Watercolor",
 	// "OpenStreetMap.Mapnik",
 	// "OpenStreetMap.DE",
 	// "Esri.WorldImagery"
@@ -22,12 +29,13 @@ var data={}, layers={}, fills =[
 ];
 
 var colorScale = d3.scale.quantize().domain([moment("July 2013").unix(), moment("May 2014").unix()]).range(colorbrewer.PuBu[9].slice(4));
-d3.json("json/lort.json", dealwithData);
+var svColorScale = d3.scale.quantize().domain([1403896539 , 1403898126]).range(colorbrewer.OrRd[9].slice(4));
+d3.json("json/2014-07-30.sv.json", dealwithData);
 
 function dealwithData(oa){
   console.log(oa.features[0]);
-	data.json= oa.features.map(function(v){
-    return [v.geometry.coordinates[0],v.geometry.coordinates[1], v.properties.thumbnail, v.properties.created_time];
+	data.json = oa.features.map(function(v){
+    return [v.geometry.coordinates[0],v.geometry.coordinates[1], v.properties.thumbnail, v.properties.created_time, v.properties.pgid];
 	});
 
   //centroids();
@@ -44,27 +52,35 @@ function points(index, chunkSize){
   //console.log(index,"of",data.json.length);
   var dataSlice = data.json.slice(index,index+chunkSize);
   if(dataSlice.length === 0) {
-    // all done, start over!
-    setTimeout(function(){points(0,chunkSize);},1);
+    // all done! start over?
+    //setTimeout(function(){points(0,chunkSize);},1);
+    d3.json("json/cville.json", dealwithData);
     return;
   }
   var bounds = m.getBounds();
   dataSlice = _.filter(dataSlice,function(v){
     return bounds.contains(L.latLng(v[0],v[1]));
   });
-  layers.points = L.layerGroup(dataSlice.map(function(v){
+
+  //!
+  dataSlice = _.filter(dataSlice, function(v,i){
+    return !(i%4);
+  });
+  //!
+  var newLayer = dataSlice.map(function(v){
     var hasThumb = v[2] ? true : false;
     var c = L.circleMarker(L.latLng(v[0],v[1]),{
       radius:2,
       stroke:false,
       fillOpacity:0.4,
       clickable:hasThumb,
-      color:colorScale(moment(v[3]).unix())
+      color: v[2] ? colorScale(moment.unix(v[3]).unix()) : svColorScale(moment(v[3]).unix())
     });
     if(hasThumb)
-      c.bindPopup("<img src='"+v[2]+"'/><br/><small>"+moment.unix(v[3]).format('MMM Do YYYY, h:mm a')+"</small>+<br/><small>"+v[0]+", "+v[1]+"</small>",{maxWidth:150,minWidth:150,maxHeight:150});
+      c.bindPopup("<img src='"+v[2]+"'/><br/><small>"+moment(v[3]).format('MMM Do YYYY, h:mm a')+"</small><br/><small>+"+v[0].toFixed(5)+", "+v[1].toFixed(5)+"</small><br/>"+v[4],{maxWidth:150,minWidth:150,maxHeight:150});
     return c;
-	}));
+  });
+  layers.points = L.layerGroup(newLayer);
   layers.points.addTo(m);
   setTimeout(function(){points(index+chunkSize,chunkSize);},1);
 }
