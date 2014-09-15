d456abad1b30edd60de1ac9ed994b6cfda08d152
Michael Holroyd <meekohi@gmail.com>
2014-09-15 16:10:15 -0400

    click for geosearch

--- js/search.js
+++ js/search.js
@@ -1,6 +1,47 @@
 var queryTimer = null;
 var searchLayer = null;
 
+m.on('click',function(e){
+  console.log(e);
+  $.ajax({
+    url: "http://localhost:9200/geotagged/_search",
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify({
+      size: 10000,
+      "query" : {
+        "match_all" : {}
+      },
+      "filter" : {
+        "geo_distance" : {
+          "distance" : "100m",
+          "distance_type": "plane",
+          "geo" : [e.latlng.lat,e.latlng.lng]
+        }
+      }
+    }),
+    success: function(data, textStatus){
+      console.log(data);
+      points = data.hits.hits.map(function(v){
+        var gram = v._source;
+        var c = L.circleMarker(L.latLng(gram.location.latitude, gram.location.longitude),{
+          radius:3,
+          stroke:true,
+          fillOpacity:1.0,
+          clickable:true,
+          fillColor: "rgb(240, 240, 200)",
+          color: "rgb(0, 0, 0)",
+          zIndexOffset: 10
+        });
+        c.bindPopup("<a href='"+gram.link+"'><img src='"+gram.images.thumbnail.url+"'/></a><br/><small>"+gram.user.full_name+" ("+gram.user.username+")</small>",{maxWidth:150,minWidth:150,maxHeight:800});
+        return c;
+      });
+      searchLayer = L.layerGroup(points);
+      m.addLayer(searchLayer);
+    }
+  });
+});
+
 $("#search").keypress(function(){
   console.log("keypress");
   clearTimeout(queryTimer);
